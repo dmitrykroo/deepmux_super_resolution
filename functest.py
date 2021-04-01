8fbca7109ed5a5682204e2acadef38465ed539a3
dmitrykroo <47831268+dmitrykroo@users.noreply.github.com>
2021-04-01 14:33:33 +0300

    Delete functest.py

--- functest.py
+++
@@ -1,9 +0,0 @@
-from func_esrgan import superres
-
-with open('LR/baboon.png', 'rb') as f:
-    data = f.read()
-    
-data_hr = superres(data)
-
-with open('baboon_hr.png', 'wb') as f:
-    f.write(data_hr)
